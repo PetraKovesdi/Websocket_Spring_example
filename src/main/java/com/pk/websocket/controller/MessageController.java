198569ee340358c6e82a87187dafbcda7b04eecd
Petra Kövesdi <kovesdi.petra@gmail.com>
2021-10-10 00:49:19 +0200

    Fix for message controller.

--- src/main/java/com/pk/websocket/controller/MessageController.java
+++ src/main/java/com/pk/websocket/controller/MessageController.java
@@ -6,6 +6,7 @@ import com.pk.websocket.storage.UserStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class MessageController {
         System.out.println("Handle sent message is " + message + " to " + to);
         boolean isExists = UserStorage.getInstance().getUsers().contains(to);
         if (isExists){
-            simpleMessagingTemplate.convertAndSend("topic/messages/" + to, message);
+            simpleMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
         }
     }
 
